c5842a57956cd4eee0d32e3aaaa7c94c3ae86799
Neelam Arunkumar <57699840+arunkumarneelam015000@users.noreply.github.com>
2021-09-06 17:38:03 +0530

    Update settings.py

--- employee_data/settings.py
+++ employee_data/settings.py
@@ -127,6 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-heroku config:set DISABLE_COLLECTSTATIC=1
 
 
